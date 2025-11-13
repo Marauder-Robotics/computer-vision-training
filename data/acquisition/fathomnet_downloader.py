1fec338dab3d06b8c4bdfaafc47203553b315048
miles <subsailor.production@gmail.com>
2025-11-13 17:49:55 +0100

    .

--- data/acquisition/fathomnet_downloader.py
+++ data/acquisition/fathomnet_downloader.py
@@ -294,7 +294,7 @@ class FathomNetDownloader:
         # Get boxes
         print(img)
         boxes = img.get('boundingBoxes')
-        print('Boxes for: {img.get('uuid')}')
+        print(f'Boxes for: {img.get('uuid')}')
         print(boxes)
 
         # Get image dimensions
