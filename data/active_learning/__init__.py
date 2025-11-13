a76d7a34e2c4af762484f218ac8bca527c7b2a2f
miles <subsailor.production@gmail.com>
2025-11-13 12:48:00 +0100

    v2- overhaul of initial commit

--- data/active_learning/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Active Learning Module - Annotation workflow with Mindy Services"""
-from .mindy_services_handler import MindyServicesHandler
-
-__all__ = ["MindyServicesHandler"]
--- deployment/nano/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Jetson Nano Deployment - Edge deployment for energy-efficient inference"""
-__all__ = []
